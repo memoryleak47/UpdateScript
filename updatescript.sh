308de37149b328f8d3d5d86768e49d6491c72951
memoryleak47 <memoryleak47@protonmail.ch>
2016-08-01 23:21:52 +0200

    now Telegram / Tor-Browser are installed properly; ...

--- updatescript.sh
+++ updatescript.sh
@@ -2,10 +2,27 @@
 
 function _install()
 {
-	if [ $(dpgk -l | grep "$1") ]; then
-		sudo apt upgrade "$1"
+	echo -e "\nInstalling $1..."
+	if [ "$(dpkg -l "$1")" ]; then
+		if [ "$(apt list --upgradeable | grep $1)" ]; then
+			sudo apt upgrade "$1"
+		fi
 	else
 		sudo apt install "$1"
+	fi
+}
+
+function _build()
+{
+	echo -e "\nInstalling $1..."
+	if [ "$(dpkg -l | grep "$1")" ]; then
+		if [ "$(apt list --upgradeable | grep $1)" ]; then
+			sudo apt-get upgrade $1
+		fi
+	else
+		sudo add-apt-repository -y $2
+		sudo apt-get update
+		sudo apt-get install telegram
 	fi
 }
 
@@ -16,15 +33,21 @@ function _mkdir()
 	fi
 }
 
-echo "Installing nice software..."
 _install git
 _install vim
 
-echo "Setting up launcher..."
+_build telegram ppa:atareao/telegram
+_build tor-browser ppa:webupd8team/tor-browser
+
+echo -e "\nSetting up launcher..."
 dconf write '/org/compiz/profiles/unity/plugins/unityshell/icon-size' '24'
 dconf write '/com/canonical/unity/launcher/favorites' "['application://org.gnome.Nautilus.desktop', 'application://gnome-terminal.desktop', 'application://firefox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
 
-echo "Setting up file system"
+echo -e "\nSetting up Workspaces"
+dconf write '/org/compiz/profiles/unity/plugins/core/hsize' '2'
+dconf write '/org/compiz/profiles/unity/plugins/core/vsize' '2'
+
+echo -e "\nSetting up file system"
 _mkdir "$HOME/projects"
 _mkdir "$HOME/projects/mind"
 _mkdir "$HOME/projects/other"
@@ -35,10 +58,5 @@ _mkdir "$HOME/projects/learn"
 
 _mkdir "$HOME/apps"
 
-echo "Installing Telegram..."
-# TODO
 
-echo "Installing Tor..."
-# TODO
-
-echo "Loading Firefox Plugins..."
+echo -e "\nLoading Firefox Plugins..."
