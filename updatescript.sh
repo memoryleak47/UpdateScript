06e29122ff1ebcfda015e106a104e0aec707176e
memoryleak47 <memoryleak47@protonmail.ch>
2017-05-29 17:00:32 +0200

    + no-config mode

--- updatescript.sh
+++ updatescript.sh
@@ -43,43 +43,45 @@ _install libsfml-dev
 _build telegram ppa:atareao/telegram
 _build tor-browser ppa:webupd8team/tor-browser
 
-echo -e "\nSetting up launcher..."
-dconf write '/org/compiz/profiles/unity/plugins/unityshell/icon-size' '24'
-dconf write '/com/canonical/unity/launcher/favorites' "['application://org.gnome.Nautilus.desktop', 'application://terminator.desktop', 'application://firefox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+if [ "a$1" != "ano-config" ]; then
+	echo -e "\nSetting up launcher..."
+	dconf write '/org/compiz/profiles/unity/plugins/unityshell/icon-size' '24'
+	dconf write '/com/canonical/unity/launcher/favorites' "['application://org.gnome.Nautilus.desktop', 'application://terminator.desktop', 'application://firefox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
 
-echo -e "\nSetting up Workspaces"
-dconf write '/org/compiz/profiles/unity/plugins/core/hsize' '2'
-dconf write '/org/compiz/profiles/unity/plugins/core/vsize' '2'
+	echo -e "\nSetting up Workspaces"
+	dconf write '/org/compiz/profiles/unity/plugins/core/hsize' '2'
+	dconf write '/org/compiz/profiles/unity/plugins/core/vsize' '2'
 
-dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-left' "['<Control><Alt>H']"
-dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-down' "['<Control><Alt>J']"
-dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-right' "['<Control><Alt>L']"
-dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-up' "['<Control><Alt>K']"
+	dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-left' "['<Control><Alt>H']"
+	dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-down' "['<Control><Alt>J']"
+	dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-right' "['<Control><Alt>L']"
+	dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-up' "['<Control><Alt>K']"
 
-dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-left' "['<Control><Shift><Alt>H']"
-dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-down' "['<Control><Shift><Alt>J']"
-dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-right' "['<Control><Shift><Alt>L']"
-dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-up' "['<Control><Shift><Alt>K']"
+	dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-left' "['<Control><Shift><Alt>H']"
+	dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-down' "['<Control><Shift><Alt>J']"
+	dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-right' "['<Control><Shift><Alt>L']"
+	dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-up' "['<Control><Shift><Alt>K']"
 
-dconf write '/org/gnome/settings-daemon/plugins/media-keys/screensaver' "'disabled'" # Also used <Control><Alt>L
+	dconf write '/org/gnome/settings-daemon/plugins/media-keys/screensaver' "'disabled'" # Also used <Control><Alt>L
 
-echo -e "\nSetting up file system"
-_mkdir "$HOME/projects"
-_mkdir "$HOME/projects/mind"
-_mkdir "$HOME/projects/other"
-_mkdir "$HOME/projects/app"
-_mkdir "$HOME/projects/util"
-_mkdir "$HOME/projects/game"
-_mkdir "$HOME/projects/learn"
+	echo -e "\nSetting up file system"
+	_mkdir "$HOME/projects"
+	_mkdir "$HOME/projects/mind"
+	_mkdir "$HOME/projects/other"
+	_mkdir "$HOME/projects/app"
+	_mkdir "$HOME/projects/util"
+	_mkdir "$HOME/projects/game"
+	_mkdir "$HOME/projects/learn"
 
-_mkdir "$HOME/apps"
+	_mkdir "$HOME/apps"
 
-echo -e "\nLoading Firefox Plugins... TODO"
-echo -e "Setting up Terminal Profiles... TODO"
+	echo -e "\nLoading Firefox Plugins... TODO"
+	echo -e "Setting up Terminal Profiles... TODO"
 
-mkdir ~/.config/terminator &>/dev/null
+	mkdir ~/.config/terminator &>/dev/null
 
-echo """
+	echo "Configuring Terminator..."
+	echo '''
 [global_config]
 [keybindings]
   broadcast_all = None
@@ -145,4 +147,6 @@ echo """
 [profiles]
   [[default]]
     cursor_color = "#aaaaaa"
-""" > ~/.config/terminator/config
+''' > ~/.config/terminator/config
+
+fi
