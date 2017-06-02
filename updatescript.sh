7f3915c9e349239c54ca6fdd1abd066a5c36e576
memoryleak47 <memoryleak47@protonmail.ch>
2017-06-02 17:24:20 +0200

    +custom shortcuts!

--- updatescript.sh
+++ updatescript.sh
@@ -48,10 +48,11 @@ if [ "a$1" != "ano-config" ]; then
 	dconf write '/org/compiz/profiles/unity/plugins/unityshell/icon-size' '24'
 	dconf write '/com/canonical/unity/launcher/favorites' "['application://org.gnome.Nautilus.desktop', 'application://terminator.desktop', 'application://firefox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
 
-	echo -e "\nSetting up Workspaces"
+	echo -e "\nSetting up Workspaces..."
 	dconf write '/org/compiz/profiles/unity/plugins/core/hsize' '2'
 	dconf write '/org/compiz/profiles/unity/plugins/core/vsize' '2'
 
+	echo -e "\nSetting up shortcuts..."
 	dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-left' "['<Control><Alt>H']"
 	dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-down' "['<Control><Alt>J']"
 	dconf write '/org/gnome/desktop/wm/keybindings/switch-to-workspace-right' "['<Control><Alt>L']"
@@ -62,7 +63,30 @@ if [ "a$1" != "ano-config" ]; then
 	dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-right' "['<Control><Shift><Alt>L']"
 	dconf write '/org/gnome/desktop/wm/keybindings/move-to-workspace-up' "['<Control><Shift><Alt>K']"
 
-	dconf write '/org/gnome/settings-daemon/plugins/media-keys/screensaver' "'disabled'" # Also used <Control><Alt>L
+	# Remove conflicting
+	dconf write '/org/gnome/settings-daemon/plugins/media-keys/screensaver' "'disabled'"
+	dconf write '/org/gnome/settings-daemon/plugins/media-keys/terminal' "'disabled'"
+
+	# Setup custom keybindings
+	dconf write '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings' "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/']"
+
+	set_shortcut() {
+		dconf write "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$4/name" "'$1'"
+		dconf write "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$4/command" "'$2'"
+		dconf write "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$4/binding" "'$3'"
+	}
+
+	set_shortcut gimp "gimp" "<Primary><Alt>g" 0
+	set_shortcut firefox "firefox" "<Primary><Alt>w" 1
+	set_shortcut telegram "/opt/telegram/telegram --" "<Primary><Alt>t" 2
+	set_shortcut terminator "terminator" "<Primary><Alt>c" 3
+	set_shortcut amarok "amarok" "<Primary><Alt>a" 4
+
+	set_shortcut system-monitor "gnome-system-monitor" "<Primary><Alt>m" 5
+	set_shortcut system-settings "unity-control-center" "<Primary><Alt>s" 6
+
+	set_shortcut poweroff "poweroff" "<Primary><Alt>End" 7
+	set_shortcut reboot "reboot" "<Primary><Alt><Shift>End" 8
 
 	echo -e "\nSetting up file system"
 	_mkdir "$HOME/projects"
@@ -78,9 +102,8 @@ if [ "a$1" != "ano-config" ]; then
 	echo -e "\nLoading Firefox Plugins... TODO"
 	echo -e "Setting up Terminal Profiles... TODO"
 
-	mkdir ~/.config/terminator &>/dev/null
-
 	echo "Configuring Terminator..."
+	mkdir ~/.config/terminator &>/dev/null
 	echo '''
 [global_config]
 [keybindings]
